4c43af745bc9d95333a829009124a5cd6c0a9015
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2019-11-20 17:29:49 +0700

    initialize assignment
    - setup phalcon ide stub
    - sql dump
    - route
    
    Signed-off-by: Dandy Naufaldi <dandy.naufaldi@gmail.com>

---
+++ apps/modules/idea/config/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+$namespace = 'Idy\Idea\Controllers\Web';
+
+$router->addGet('/', [
+    'namespace' => $namespace,
+    'module' => 'idea',
+    'controller' => 'idea',
+    'action' => 'index'
+]);
+
+$router->add('/idea/add', [
+    'namespace' => $namespace,
+    'module' => 'idea',
+    'controller' => 'idea',
+    'action' => 'add'
+]);
+
+$router->addPost('/idea/vote/:params', [
+    'namespace' => $namespace,
+    'module' => 'idea',
+    'controller' => 'idea',
+    'action' => 'vote'
+]);
+
+$router->add('/idea/rate/:params', [
+    'namespace' => $namespace,
+    'module' => 'idea',
+    'controller' => 'idea',
+    'action' => 'rate'
+]);
---
+++ idy.sql
@@ -0,0 +1,63 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: idy
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ideas`
+--
+
+DROP TABLE IF EXISTS `ideas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ideas` (
+  `id` varchar(36) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `votes` int(11) DEFAULT NULL,
+  `author_name` varchar(45) NOT NULL,
+  `author_email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ratings`
+--
+
+DROP TABLE IF EXISTS `ratings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idea_id` varchar(36) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `value` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idea_id_idx` (`idea_id`),
+  CONSTRAINT `idea_id` FOREIGN KEY (`idea_id`) REFERENCES `ideas` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-20 17:27:55
