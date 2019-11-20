21e7cdddf6430ec1efd51adc72df1df39d63247f
Dandy Naufaldi <dandy.naufaldi@gmail.com>
2019-11-20 20:55:42 +0700

    SqlIdeaRepository done byId()
    Signed-off-by: Dandy Naufaldi <dandy.naufaldi@gmail.com>

--- apps/modules/idea/domain/model/IdeaRepository.php
+++ apps/modules/idea/domain/model/IdeaRepository.php
@@ -4,7 +4,7 @@ namespace Idy\Idea\Domain\Model;
 
 interface IdeaRepository
 {
-    public function byId(IdeaId $id);
-    public function save(Idea $idea);
-    public function allIdeas();
+    public function byId(IdeaId $id) : ?Idea;
+    public function save(Idea $idea) : void;
+    public function allIdeas() : ?array;
 }
--- apps/modules/idea/infrastructure/SqlIdeaRepository.php
+++ apps/modules/idea/infrastructure/SqlIdeaRepository.php
@@ -2,28 +2,132 @@
 
 namespace Idy\Idea\Infrastructure;
 
+use Exception;
+use Phalcon\Di;
+use Phalcon\Db\Column;
+
+use Idy\Common\Exceptions\ResourceNotFoundException;
+use Idy\Idea\Domain\Model\Idea;
+use Idy\Idea\Domain\Model\Author;
+use Idy\Idea\Domain\Model\IdeaRepository;
+use Idy\Idea\Domain\Model\IdeaId;
+use Idy\Idea\Domain\Model\Rating;
+
 class SqlIdeaRepository implements IdeaRepository
 {
     private $ideas;
-
-    public function __construct()
+    private $db;
+    public function __construct($di)
     {
         $this->ideas = array();
+        $this->db = $di->get('db');
     }
 
-    public function byId(IdeaId $id)
+    private function fetchIdeaById(IdeaId $id) : array
+    {
+        $query = "
+        SELECT id, title, description, votes, author_name, author_email 
+        FROM `ideas `
+        WHERE id=:id
+        ";
+        $statement = $this->db->prepare($query);
+        $params = [
+            'id' => $id->id()
+        ];
+        $types = [
+            'id' => Column::BIND_PARAM_INT
+        ];
+        $idea = $this->db->executePrepared($statement, $params, $types);
+        
+        if ($idea->rowCount() == 0)
+        {
+            throw new ResourceNotFoundException("Idea with ID ".$id->id()." not exist");
+        }
+        $idea = $idea->fetch();
+        return $idea;
+    }
+
+    private function fetchRatingsByIdeaId(IdeaId $id) : array
+    {
+        $query = "
+        SELECT name, value
+        FROM `ratings`
+        WHERE idea_id=:idea_id
+        ";
+        $statement = $this->db->prepare($query);
+        $param = [
+            'idea_id' => $id->id()
+        ];
+        $types = [
+            'idea_id' => Column::BIND_PARAM_INT
+        ];
+        $ratings = $this->db->executePrepared($statement, $param, $types)->fetchAll();
+        return $ratings;
+    }
+
+    public function byId(IdeaId $id) : ?Idea
+    {  
+        $idea = $this->fetchIdeaById($id);
+        $ratings = $this->fetchRatingsByIdeaId($id);
+        
+        $ratingObjects = array();
+        foreach ($ratings as $rating) {
+            $ratingObject = new Rating($rating["name"], $rating["value"]);
+            array_push($ratingObjects, $ratingObject);
+        }
+        
+        $author = new Author($idea["author_name"], $idea["author_email"]);
+        $idea = new Idea(
+            $id, 
+            $idea["title"], 
+            $idea["description"], 
+            $author,
+            $idea["votes"],
+            $ratingObjects
+        );
+        return $idea;
+    }
+
+    private function isExist(IdeaId $id) : bool
+    {
+        $query = "
+        SELECT id from `ideas`
+        WHERE id=:id
+        ";
+        $statement = $this->db->prepare($query);
+        $params = [
+            'id' => $id->id()
+        ];
+        $types = [
+            'id' => Column::BIND_PARAM_INT
+        ];
+        $idea = $this->db->executePrepared($statement, $params, $types);
+        
+        if ($idea->rowCount() == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private function create(Idea $idea)
     {
 
     }
 
-    public function save(Idea $idea)
+    private function update(Idea $idea)
     {
 
     }
 
-    public function allIdeas()
+    public function save(Idea $idea) : void
     {
+        return;
+    }
 
+    public function allIdeas() : array
+    {
+        return array();
     }
     
 }
